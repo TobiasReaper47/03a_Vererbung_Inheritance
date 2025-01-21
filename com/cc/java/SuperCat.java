88524e56a395d23bf26ecb268500b9d1addc0ed3
TobiasReaper47 <tobiasreaper273@gmail.com>
2025-01-21 12:40:11 +0100

    Supercat Start

---
+++ com/cc/java/SuperCat.java
@@ -0,0 +1,29 @@
+package com.cc.java;
+
+public class SuperCat {
+
+    private String firstName;
+    private String furColor;
+    protected int age;
+        
+    public SuperCat(String firstName, String furColor, int age) {
+        this.firstName = firstName;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+    /** Getter */
+    public String getStringAttributes(String op) {
+        switch (op) {
+          case "#name":
+              return firstName;
+          case "#color":
+              return furColor;
+          default:
+              return "ERROR";
+        }
+      }
+    
+
+
+}
