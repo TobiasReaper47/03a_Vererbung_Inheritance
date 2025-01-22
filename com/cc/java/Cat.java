bb4b5d39b0c6ce9904e505c4563e9df4ec8e842c
TobiasReaper47 <tobiasreaper273@gmail.com>
2025-01-22 08:27:01 +0100

    Supercat done

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,37 +1,19 @@
 package com.cc.java;
 
-public class Cat {
+public class Cat extends SuperCat{
 
-    private String firstName;
-    private String furColor;
-    private int age;
-       
-    
     private int counter;
 
     public Cat(String firstName, String furColor, int age) {
-        this.firstName = firstName;
-        this.furColor = furColor;
-        this.age = age;
+       super(firstName, furColor, age);
     }
 
-    /** Getter */
-    public String getStringAttributes(String op) {
-      switch (op) {
-        case "#name":
-            return firstName;
-        case "#color":
-            return furColor;
-        default:
-            return "ERROR";
-      }
-    }
 
     public String getAge() {
-           return checkCompliance();    
-      
+           return checkCompliance();
     }
-   
+
+    
     private String checkCompliance(){
 
        counter++;
--- com/cc/java/Tomcat.java
+++ com/cc/java/Tomcat.java
@@ -1,37 +1,17 @@
 package com.cc.java;
 
-public class Tomcat {
+public class Tomcat extends SuperCat{
 
-    private String firstName;
-    private String furColor;
-    private int age;
-   
-
+  
     public Tomcat(String firstName, String furColor, int age) {
-        this.firstName = firstName;
-        this.furColor = furColor;
-        this.age = age;
+       super(firstName, furColor, age);
     }
 
-    /** Getter */
-    public String getStringAttributes(String op) {
-      switch (op) {
-        case "#name":
-            return firstName;
-        case "#color":
-            return furColor;
-        default:
-            return "ERROR";
-      }
-    }
 
     public String getAge() {
         return Integer.toString(age); 
-        }
     }
 
-    
- 
-
+}
 
 
